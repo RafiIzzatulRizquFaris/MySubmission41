e72de039defb7c243dbc868adda7f34895195ab5
quifar <42423920+RafiIzzatulRizquFaris@users.noreply.github.com>
2019-09-17 06:53:44 +0700

    Change Action Bar Title on ReminderActivity

--- app/src/main/java/com/example/mysubmission41/ReminderActivity.java
+++ app/src/main/java/com/example/mysubmission41/ReminderActivity.java
@@ -5,14 +5,22 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.os.Bundle;
 import android.widget.Switch;
 
+import java.util.Objects;
+
 public class ReminderActivity extends AppCompatActivity {
 
     Switch aSwitchDaily, aSwitchRelease;
+
+    private void setActionBarTitle(){
+        Objects.requireNonNull(getSupportActionBar()).setTitle(getString(R.string.set_reminder));
+    }
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_reminder);
+
+        setActionBarTitle();
 
         aSwitchDaily = findViewById(R.id.sw_daily_reminder);
         aSwitchRelease = findViewById(R.id.sw_release_reminder);
