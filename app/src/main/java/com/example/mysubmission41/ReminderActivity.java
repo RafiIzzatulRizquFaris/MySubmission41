f618f29abf0a019b5226891a4f0963b323516817
quifar <42423920+RafiIzzatulRizquFaris@users.noreply.github.com>
2019-09-17 06:31:07 +0700

    Put id for Switch

--- app/src/main/java/com/example/mysubmission41/ReminderActivity.java
+++ app/src/main/java/com/example/mysubmission41/ReminderActivity.java
@@ -3,12 +3,19 @@ package com.example.mysubmission41;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.Switch;
 
 public class ReminderActivity extends AppCompatActivity {
+
+    Switch aSwitchDaily, aSwitchRelease;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_reminder);
+
+        aSwitchDaily = findViewById(R.id.sw_daily_reminder);
+        aSwitchRelease = findViewById(R.id.sw_release_reminder);
+
     }
 }
