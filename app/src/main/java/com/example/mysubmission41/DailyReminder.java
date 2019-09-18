e96b06ee18eca8615f55e4b3a592d6c14f5fc544
quifar <42423920+RafiIzzatulRizquFaris@users.noreply.github.com>
2019-09-18 07:17:15 +0700

    Adding DailyReminder Class

---
+++ app/src/main/java/com/example/mysubmission41/DailyReminder.java
@@ -0,0 +1,12 @@
+package com.example.mysubmission41;
+
+import android.content.BroadcastReceiver;
+import android.content.Context;
+import android.content.Intent;
+
+public class DailyReminder extends BroadcastReceiver {
+    @Override
+    public void onReceive(Context context, Intent intent) {
+
+    }
+}
