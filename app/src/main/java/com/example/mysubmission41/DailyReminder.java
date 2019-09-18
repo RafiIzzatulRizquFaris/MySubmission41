087663c3c3a7fbf3655193e0b0ed107bf6afcc7a
quifar <42423920+RafiIzzatulRizquFaris@users.noreply.github.com>
2019-09-18 07:18:38 +0700

    Adding DailyReminder Class + KEY

--- app/src/main/java/com/example/mysubmission41/DailyReminder.java
+++ app/src/main/java/com/example/mysubmission41/DailyReminder.java
@@ -5,6 +5,11 @@ import android.content.Context;
 import android.content.Intent;
 
 public class DailyReminder extends BroadcastReceiver {
+
+    public static final int NOTIFICATION_ID = 10;
+    public static String CHANNEL_ID = "channel_01";
+    public static CharSequence CHANNEL_NAME = "daily channel";
+
     @Override
     public void onReceive(Context context, Intent intent) {
 
