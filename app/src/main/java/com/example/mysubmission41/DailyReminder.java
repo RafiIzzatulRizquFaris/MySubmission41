701a0056afd849e765b9b18417d867241aca5b75
quifar <42423920+RafiIzzatulRizquFaris@users.noreply.github.com>
2019-09-21 17:58:18 +0700

    Updated in DailyRemainder class (cancelNotification & setAlarm void method)

--- app/src/main/java/com/example/mysubmission41/DailyReminder.java
+++ app/src/main/java/com/example/mysubmission41/DailyReminder.java
@@ -1,8 +1,24 @@
 package com.example.mysubmission41;
 
+import android.app.AlarmManager;
+import android.app.Notification;
+import android.app.NotificationChannel;
+import android.app.NotificationManager;
+import android.app.PendingIntent;
+import android.app.TaskStackBuilder;
 import android.content.BroadcastReceiver;
 import android.content.Context;
 import android.content.Intent;
+import android.media.RingtoneManager;
+import android.net.Uri;
+import android.os.Build;
+
+import androidx.core.app.NotificationCompat;
+
+import com.example.mysubmission41.activity.MainActivity;
+
+import java.util.Calendar;
+import java.util.Objects;
 
 public class DailyReminder extends BroadcastReceiver {
 
@@ -12,6 +28,60 @@ public class DailyReminder extends BroadcastReceiver {
 
     @Override
     public void onReceive(Context context, Intent intent) {
+        notificationDaily(context, context.getResources().getString(R.string.message_daily_remainder), intent.getStringExtra("message"), NOTIFICATION_ID);
+    }
 
+    private void notificationDaily(Context context, String title, String message, int notificationid) {
+        NotificationManager notificationManager = (NotificationManager) context.getSystemService(Context.NOTIFICATION_SERVICE);
+        Intent intent = new Intent(context, MainActivity.class);
+        PendingIntent pendingIntent = TaskStackBuilder.create(context)
+                .addNextIntent(intent)
+                .getPendingIntent(NOTIFICATION_ID, PendingIntent.FLAG_UPDATE_CURRENT);
+        Uri defaultNotif = RingtoneManager.getDefaultUri(RingtoneManager.TYPE_NOTIFICATION);
+        NotificationCompat.Builder mBuilder = new NotificationCompat.Builder(context, CHANNEL_ID)
+                .setContentIntent(pendingIntent)
+                .setSmallIcon(R.drawable.ic_notifications_black_24dp)
+                .setContentTitle(title)
+                .setContentText(message)
+                .setVibrate(new long[] {1000, 1000, 1000, 1000})
+                .setSound(defaultNotif);
+
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
+            NotificationChannel channel = new NotificationChannel(CHANNEL_ID, CHANNEL_NAME, NotificationManager.IMPORTANCE_DEFAULT);
+            mBuilder.setChannelId(CHANNEL_ID);
+            if (notificationManager != null) {
+                notificationManager.createNotificationChannel(channel);
+            }
+        }
+
+        Notification notification = mBuilder.build();
+
+        if (notificationManager != null) {
+            notificationManager.notify(notificationid, notification);
+            mBuilder.setAutoCancel(true);
+        }
+    }
+
+    public void cancelNotification(Context context) {
+        AlarmManager alarmManager = (AlarmManager) context.getSystemService(Context.ALARM_SERVICE);
+        Intent intent = new Intent(context, DailyReminder.class);
+        PendingIntent pendingIntent = PendingIntent.getBroadcast(context, NOTIFICATION_ID, intent, 0);
+        (alarmManager).cancel(pendingIntent);
+    }
+
+    public void setAlarm(Context context, String type, String time, String message) {
+        cancelNotification(context);
+        AlarmManager alarmManager = (AlarmManager) context.getSystemService(Context.ALARM_SERVICE);
+        Intent intent = new Intent(context, DailyReminder.class);
+        intent.putExtra("message", message);
+        intent.putExtra("type", type);
+        String timeArray[] = time.split(":");
+        Calendar calendar = Calendar.getInstance();
+        calendar.set(Calendar.HOUR_OF_DAY, Integer.parseInt(timeArray[0]));
+        calendar.set(Calendar.MINUTE, Integer.parseInt(timeArray[1]));
+        calendar.set(Calendar.SECOND, 0);
+
+        PendingIntent pendingIntent = PendingIntent.getBroadcast(context, NOTIFICATION_ID, intent, 0);
+        Objects.requireNonNull(alarmManager).setInexactRepeating(AlarmManager.RTC_WAKEUP, calendar.getTimeInMillis(), AlarmManager.INTERVAL_DAY, pendingIntent);
     }
 }
