74bc4a168244c4811658f4b211e34ad265d035c3
quifar <42423920+RafiIzzatulRizquFaris@users.noreply.github.com>
2019-09-24 06:47:30 +0700

    Added 2 activity for search navigation, add word for project dictionaries

---
+++ app/src/main/java/com/example/mysubmission41/MovieSearchActivity.java
@@ -0,0 +1,14 @@
+package com.example.mysubmission41;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MovieSearchActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_movie_search);
+    }
+}
---
+++ app/src/main/java/com/example/mysubmission41/TvSearchActivity.java
@@ -0,0 +1,14 @@
+package com.example.mysubmission41;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class TvSearchActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_tv_search);
+    }
+}
