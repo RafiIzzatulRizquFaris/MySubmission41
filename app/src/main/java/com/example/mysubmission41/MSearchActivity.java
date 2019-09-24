437af5e062ddf58bef98659dbbed832f2497d75b
quifar <42423920+RafiIzzatulRizquFaris@users.noreply.github.com>
2019-09-24 07:56:36 +0700

    Adding id rv to MSearchActivity + set title action bar

--- app/src/main/java/com/example/mysubmission41/MSearchActivity.java
+++ app/src/main/java/com/example/mysubmission41/MSearchActivity.java
@@ -1,21 +1,38 @@
 package com.example.mysubmission41;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.recyclerview.widget.RecyclerView;
 
 import android.os.Bundle;
 import android.util.Log;
+
+import com.example.mysubmission41.adapter.MovieAdapter;
+import com.example.mysubmission41.pojo.Movie;
+
+import java.util.ArrayList;
+import java.util.Objects;
 
 public class MSearchActivity extends AppCompatActivity {
 
     public static final String EXTRA_QUERY = "search_movie";
     private static final String TAG = "MSearchActivity";
 
+    RecyclerView recyclerView;
+    MovieAdapter movieAdapter;
+    ArrayList<Movie> movieArrayList = new ArrayList<>();
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_msearch);
-
         String query = getIntent().getStringExtra(EXTRA_QUERY);
         Log.d(TAG, "search: " + query);
+        setActionBarTitle(query);
+
+        recyclerView = findViewById(R.id.rv_search_movie);
+    }
+
+    private void setActionBarTitle(String query){
+        Objects.requireNonNull(getSupportActionBar()).setTitle(query);
     }
 }
