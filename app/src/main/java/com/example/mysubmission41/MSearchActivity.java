e7da988e11235b942fdb0bd0ccb1b0ae224b24ee
quifar <42423920+RafiIzzatulRizquFaris@users.noreply.github.com>
2019-09-24 07:37:54 +0700

    Update MSearchActivity.java

--- app/src/main/java/com/example/mysubmission41/MSearchActivity.java
+++ app/src/main/java/com/example/mysubmission41/MSearchActivity.java
@@ -3,10 +3,12 @@ package com.example.mysubmission41;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.util.Log;
 
 public class MSearchActivity extends AppCompatActivity {
 
     public static final String EXTRA_QUERY = "search_movie";
+    private static final String TAG = "MSearchActivity";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -14,5 +16,6 @@ public class MSearchActivity extends AppCompatActivity {
         setContentView(R.layout.activity_msearch);
 
         String query = getIntent().getStringExtra(EXTRA_QUERY);
+        Log.d(TAG, "search: " + query);
     }
 }
