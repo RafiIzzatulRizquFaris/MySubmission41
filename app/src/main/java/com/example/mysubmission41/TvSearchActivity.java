92ff610719f1775b1933021659156fb67b852cf0
quifar <42423920+RafiIzzatulRizquFaris@users.noreply.github.com>
2019-09-24 07:06:20 +0700

    adding search manager to TvSearchActivity class

--- app/src/main/java/com/example/mysubmission41/TvSearchActivity.java
+++ app/src/main/java/com/example/mysubmission41/TvSearchActivity.java
@@ -2,7 +2,12 @@ package com.example.mysubmission41;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.app.SearchManager;
+import android.content.Context;
 import android.os.Bundle;
+import android.view.Menu;
+import android.widget.SearchView;
+import android.widget.Toast;
 
 public class TvSearchActivity extends AppCompatActivity {
 
@@ -10,5 +15,28 @@ public class TvSearchActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_tv_search);
+    }
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.main, menu);
+        SearchManager searchManager = (SearchManager) getSystemService(Context.SEARCH_SERVICE);
+        if (searchManager != null){
+            SearchView searchView = (SearchView) (menu.findItem(R.id.search).getActionView());
+            searchView.setSearchableInfo(searchManager.getSearchableInfo(getComponentName()));
+            searchView.setQueryHint(getResources().getString(R.string.search_hint));
+            searchView.setOnQueryTextListener(new SearchView.OnQueryTextListener() {
+                @Override
+                public boolean onQueryTextSubmit(String query) {
+                    Toast.makeText(TvSearchActivity.this, query, Toast.LENGTH_SHORT).show();
+                    return true;
+                }
+
+                @Override
+                public boolean onQueryTextChange(String newText) {
+                    return false;
+                }
+            });
+        }
+        return true;
     }
 }
