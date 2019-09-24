f462f525f996ac940fdbbb5e2f61ac4be8c050a2
quifar <42423920+RafiIzzatulRizquFaris@users.noreply.github.com>
2019-09-24 07:28:13 +0700

    Adding MSearchActivity

---
+++ app/src/main/java/com/example/mysubmission41/MSearchActivity.java
@@ -0,0 +1,14 @@
+package com.example.mysubmission41;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MSearchActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_msearch);
+    }
+}
