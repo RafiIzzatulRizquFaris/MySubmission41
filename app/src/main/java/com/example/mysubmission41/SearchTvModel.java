dfd8820cabb88e60c11e833ca43a832229512e14
quifar <42423920+RafiIzzatulRizquFaris@users.noreply.github.com>
2019-09-24 08:36:36 +0700

    Adding SearchTvModel class

--- app/src/main/java/com/example/mysubmission41/SearchTvModel.java
+++ app/src/main/java/com/example/mysubmission41/SearchTvModel.java
@@ -1,5 +1,52 @@
 package com.example.mysubmission41;
 
-class SearchTvModel {
+import com.example.mysubmission41.pojo.TvShow;
+import com.google.gson.annotations.SerializedName;
 
+import java.util.List;
+
+class SearchTvModel {
+    @SerializedName("page")
+    private int page;
+
+    @SerializedName("total_pages")
+    private int totalPages;
+
+    public int getPage() {
+        return page;
+    }
+
+    public void setPage(int page) {
+        this.page = page;
+    }
+
+    public int getTotalPages() {
+        return totalPages;
+    }
+
+    public void setTotalPages(int totalPages) {
+        this.totalPages = totalPages;
+    }
+
+    public int getTotalResults() {
+        return totalResults;
+    }
+
+    public void setTotalResults(int totalResults) {
+        this.totalResults = totalResults;
+    }
+
+    public List<TvShow> getTvShow() {
+        return tvShow;
+    }
+
+    public void setTvShow(List<TvShow> tvShow) {
+        this.tvShow = tvShow;
+    }
+
+    @SerializedName("total_results")
+    private int totalResults;
+
+    @SerializedName("results")
+    private List<TvShow> tvShow;
 }
