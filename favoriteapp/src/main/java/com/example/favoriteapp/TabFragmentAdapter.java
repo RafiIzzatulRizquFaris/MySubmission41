35bbe061d5086ff20372b4c9f9ac15bb3d7d3e62
quifar <42423920+RafiIzzatulRizquFaris@users.noreply.github.com>
2019-09-24 11:46:18 +0700

    creating TabFragmentAdapter class to favoriteapp module

---
+++ favoriteapp/src/main/java/com/example/favoriteapp/TabFragmentAdapter.java
@@ -0,0 +1,24 @@
+package com.example.favoriteapp;
+
+import androidx.annotation.NonNull;
+import androidx.fragment.app.Fragment;
+import androidx.fragment.app.FragmentManager;
+import androidx.fragment.app.FragmentPagerAdapter;
+
+public class TabFragmentAdapter extends FragmentPagerAdapter {
+
+    public TabFragmentAdapter(@NonNull FragmentManager fm) {
+        super(fm);
+    }
+
+    @NonNull
+    @Override
+    public Fragment getItem(int position) {
+        return null;
+    }
+
+    @Override
+    public int getCount() {
+        return 0;
+    }
+}
