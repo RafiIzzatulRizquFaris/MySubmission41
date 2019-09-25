559d1c38705207b7f6c4e0f36e459773d9576246
quifar <42423920+RafiIzzatulRizquFaris@users.noreply.github.com>
2019-09-25 21:19:04 +0700

    Create FavMovieAdapt.java

---
+++ favoriteapp/src/main/java/com/example/favoriteapp/FavMovieAdapt.java
@@ -0,0 +1,31 @@
+package com.example.favoriteapp;
+
+import android.view.View;
+import android.view.ViewGroup;
+
+import androidx.annotation.NonNull;
+import androidx.recyclerview.widget.RecyclerView;
+
+public class FavMovieAdapt extends RecyclerView.Adapter<FavMovieAdapt.ViewHolder> {
+    @NonNull
+    @Override
+    public ViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
+        return null;
+    }
+
+    @Override
+    public void onBindViewHolder(@NonNull ViewHolder holder, int position) {
+
+    }
+
+    @Override
+    public int getItemCount() {
+        return 0;
+    }
+
+    public class ViewHolder extends RecyclerView.ViewHolder{
+        public ViewHolder(@NonNull View itemView) {
+            super(itemView);
+        }
+    }
+}
