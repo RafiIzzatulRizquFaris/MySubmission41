ffbf2292f67e144d138a5ae19056bdaff096e7b4
quifar <42423920+RafiIzzatulRizquFaris@users.noreply.github.com>
2019-09-25 21:33:46 +0700

    Updating FavMovieAdapt + repair id item_movie_favapp to favapp

--- favoriteapp/src/main/java/com/example/favoriteapp/FavMovieAdapt.java
+++ favoriteapp/src/main/java/com/example/favoriteapp/FavMovieAdapt.java
@@ -1,7 +1,10 @@
 package com.example.favoriteapp;
 
+import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ImageView;
+import android.widget.TextView;
 
 import androidx.annotation.NonNull;
 import androidx.recyclerview.widget.RecyclerView;
@@ -10,7 +13,8 @@ public class FavMovieAdapt extends RecyclerView.Adapter<FavMovieAdapt.ViewHolder
     @NonNull
     @Override
     public ViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
-        return null;
+        View v = LayoutInflater.from(parent.getContext()).inflate(R.layout.item_movie_favapp, parent, false);
+        return new ViewHolder(v);
     }
 
     @Override
@@ -24,8 +28,13 @@ public class FavMovieAdapt extends RecyclerView.Adapter<FavMovieAdapt.ViewHolder
     }
 
     public class ViewHolder extends RecyclerView.ViewHolder{
+        ImageView imageView;
+        TextView textView, textView2;
         public ViewHolder(@NonNull View itemView) {
             super(itemView);
+            imageView = itemView.findViewById(R.id.img_item_movie);
+            textView = itemView.findViewById(R.id.tv_item_name_movie);
+            textView2 = itemView.findViewById(R.id.movie_final_date_fav);
         }
     }
 }
