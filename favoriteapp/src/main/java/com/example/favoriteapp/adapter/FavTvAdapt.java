c66cdb559f9041f48bc66879de7215e93c975726
quifar <42423920+RafiIzzatulRizquFaris@users.noreply.github.com>
2019-09-26 07:07:16 +0700

    adding RecyclerView.Adapter to FavTvAdapt in favapp

--- favoriteapp/src/main/java/com/example/favoriteapp/adapter/FavTvAdapt.java
+++ favoriteapp/src/main/java/com/example/favoriteapp/adapter/FavTvAdapt.java
@@ -1,4 +1,31 @@
 package com.example.favoriteapp.adapter;
 
-public class FavTvAdapt {
+import android.view.View;
+import android.view.ViewGroup;
+
+import androidx.annotation.NonNull;
+import androidx.recyclerview.widget.RecyclerView;
+
+public class FavTvAdapt extends RecyclerView.Adapter<FavTvAdapt.ViewHolder> {
+    @NonNull
+    @Override
+    public ViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
+        return null;
+    }
+
+    @Override
+    public void onBindViewHolder(@NonNull ViewHolder holder, int position) {
+
+    }
+
+    @Override
+    public int getItemCount() {
+        return 0;
+    }
+
+    public class ViewHolder extends RecyclerView.ViewHolder{
+        public ViewHolder(@NonNull View itemView) {
+            super(itemView);
+        }
+    }
 }
