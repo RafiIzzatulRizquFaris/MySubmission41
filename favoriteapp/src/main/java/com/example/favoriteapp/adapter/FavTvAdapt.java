8097450330314933f31dd2a642a9976d642aefe2
quifar <42423920+RafiIzzatulRizquFaris@users.noreply.github.com>
2019-09-26 07:10:42 +0700

    adding constructor to FavTvAdapt in favapp

--- favoriteapp/src/main/java/com/example/favoriteapp/adapter/FavTvAdapt.java
+++ favoriteapp/src/main/java/com/example/favoriteapp/adapter/FavTvAdapt.java
@@ -1,12 +1,27 @@
 package com.example.favoriteapp.adapter;
 
+import android.database.Cursor;
 import android.view.View;
 import android.view.ViewGroup;
 
 import androidx.annotation.NonNull;
 import androidx.recyclerview.widget.RecyclerView;
 
+import com.example.favoriteapp.fragment.TvFragment;
+
 public class FavTvAdapt extends RecyclerView.Adapter<FavTvAdapt.ViewHolder> {
+
+    Cursor cursor;
+    TvFragment tvFragment;
+
+    public void setListTv(Cursor cursor) {
+        this.cursor = cursor;
+    }
+
+    public FavTvAdapt(TvFragment tvFragment) {
+        this.tvFragment = tvFragment;
+    }
+
     @NonNull
     @Override
     public ViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
